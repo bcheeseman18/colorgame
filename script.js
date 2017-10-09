444b039be0c33e29ac80cf23ea7c96410a6c28a7
bcheeseman18 <benjamincheeseman18@gmail.com>
2017-10-09 15:30:50 -0400

    style changes

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay'); 
 let messageDisplay = document.getElementById('message'); 
 let h1 = document.querySelector('h1'); 
+let resetButton = document.querySelector('#reset'); 
+
+resetButton.addEventListener('click', function (){
+    //generate all new colors
+    colors = generateRandomColors(6); 
+    //pick new random color
+    pickedColor = pickColor(); 
+    //change color display to match picked color
+    colorDisplay.textContent = pickedColor; 
+    //change colors of squares on page
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i]; 
+    }
+    h1.style.backgroundColor = '#232323'; 
+}); 
 
 colorDisplay.textContent = pickedColor; 
 
@@ -19,6 +34,7 @@ for(let i = 0; i <squares.length; i++) {
         //compare color to picked color
         if(clickedColor === pickedColor) {
             messageDisplay.textContent = "CORRECT"; 
+            resetButton.textContent = "PLAY AGAIN"; 
             changeColors(clickedColor); 
             h1.style.backgroundColor = clickedColor; 
         } else {
